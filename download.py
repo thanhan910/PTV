6eab3da42457139460a4467888de6c9d67efc294
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-12-25 10:26:35 +1100

    Update output directory in download.py

--- download.py
+++ download.py
@@ -5,5 +5,5 @@ import os
 if __name__ == '__main__':
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     current_dir = os.getcwd()
-    output_dir = os.path.join(current_dir, 'datasets', current_time)
+    output_dir = os.path.join(current_dir, 'downloads', current_time)
     comp.download_gtfs_zip("http://data.ptv.vic.gov.au/downloads/gtfs.zip", output_dir)
