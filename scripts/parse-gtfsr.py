595130a0f8119e0305241d8dac59f5fe8489204d
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-03-09 01:33:25 +1100

    Add script to parse dowloaded GTFS-realtime data into JSON format

---
+++ scripts/parse-gtfsr.py
@@ -0,0 +1,67 @@
+import os
+import json
+import logging
+import sys
+from google.transit import gtfs_realtime_pb2
+
+dataset_dir = '../local/realtime-data'
+
+output_dir = '../local/gtfsr-json'
+os.makedirs(output_dir, exist_ok=True)
+
+log_dir = '../local/logs'
+os.makedirs(log_dir, exist_ok=True)
+
+# Create a logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create a handler for logging to file
+file_handler = logging.FileHandler(f'{log_dir}/gtfsr2json.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# Create a handler for logging to console
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+files = [f for f in os.listdir(dataset_dir)]
+
+
+def parse_helper(entity):
+    if 'ListFields' not in dir(entity):
+        return entity
+    entity_dict = {}
+    for field in entity.ListFields():
+        field_name = field[0].name
+        if field[0].label == field[0].LABEL_REPEATED:
+            field_value = [parse_helper(item) for item in field[1]]
+        else:
+            field_value = parse_helper(field[1])
+        entity_dict[field_name] = field_value
+    return entity_dict
+
+def parse_gtfs_realtime_feed(feed):
+    return [parse_helper(entity) for entity in feed.entity]
+
+
+for f in files:
+    with open(f'{dataset_dir}/{f}', 'rb') as file:
+        try:
+            file_name = f.split('.')[0]
+            logger.info(f'Parsing file: {f}')
+            feed = gtfs_realtime_pb2.FeedMessage()
+            feed.ParseFromString(file.read())
+            data = parse_gtfs_realtime_feed(feed)
+            logger.info(f'Parse complete: {f}')
+            with open(f'{output_dir}/{file_name}.json', 'w') as out_file:
+                json.dump(data, out_file)
+                logger.info(f'Write complete: {file_name}.json')
+        except:
+            logger.error(f'Error parsing file: {f}')
+            continue
