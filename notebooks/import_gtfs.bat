4f38c66a0a06bdb24ba96a0ac30afee605124295
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-03-25 03:15:31 +1100

    Add bat file that imports gtfs to postgres

---
+++ notebooks/import_gtfs.bat
@@ -0,0 +1,161 @@
+set PGPASSWORD=postgres
+psql -U postgres -d gtfs -c "\copy agency_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_1.csv
+psql -U postgres -d gtfs -c "\copy calendar_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_1.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_1.csv
+psql -U postgres -d gtfs -c "\copy routes_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_1.csv
+psql -U postgres -d gtfs -c "\copy shapes_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_1.csv
+psql -U postgres -d gtfs -c "\copy stops_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_1.csv
+psql -U postgres -d gtfs -c "\copy stop_times_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_1.csv
+psql -U postgres -d gtfs -c "\copy trips_1 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_1.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_1.csv
+psql -U postgres -d gtfs -c "\copy agency_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_10.csv
+psql -U postgres -d gtfs -c "\copy calendar_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_10.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_10.csv
+psql -U postgres -d gtfs -c "\copy routes_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_10.csv
+psql -U postgres -d gtfs -c "\copy shapes_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_10.csv
+psql -U postgres -d gtfs -c "\copy stops_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_10.csv
+psql -U postgres -d gtfs -c "\copy stop_times_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_10.csv
+psql -U postgres -d gtfs -c "\copy trips_10 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_10.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_10.csv
+psql -U postgres -d gtfs -c "\copy agency_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_11.csv
+psql -U postgres -d gtfs -c "\copy calendar_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_11.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_11.csv
+psql -U postgres -d gtfs -c "\copy routes_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_11.csv
+psql -U postgres -d gtfs -c "\copy shapes_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_11.csv
+psql -U postgres -d gtfs -c "\copy stops_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_11.csv
+psql -U postgres -d gtfs -c "\copy stop_times_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_11.csv
+psql -U postgres -d gtfs -c "\copy trips_11 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_11.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_11.csv
+psql -U postgres -d gtfs -c "\copy agency_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_2.csv
+psql -U postgres -d gtfs -c "\copy calendar_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_2.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_2.csv
+psql -U postgres -d gtfs -c "\copy routes_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_2.csv
+psql -U postgres -d gtfs -c "\copy shapes_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_2.csv
+psql -U postgres -d gtfs -c "\copy stops_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_2.csv
+psql -U postgres -d gtfs -c "\copy stop_times_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_2.csv
+psql -U postgres -d gtfs -c "\copy trips_2 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_2.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_2.csv
+psql -U postgres -d gtfs -c "\copy agency_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_3.csv
+psql -U postgres -d gtfs -c "\copy calendar_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_3.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_3.csv
+psql -U postgres -d gtfs -c "\copy routes_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_3.csv
+psql -U postgres -d gtfs -c "\copy shapes_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_3.csv
+psql -U postgres -d gtfs -c "\copy stops_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_3.csv
+psql -U postgres -d gtfs -c "\copy stop_times_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_3.csv
+psql -U postgres -d gtfs -c "\copy trips_3 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_3.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_3.csv
+psql -U postgres -d gtfs -c "\copy agency_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_4.csv
+psql -U postgres -d gtfs -c "\copy calendar_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_4.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_4.csv
+psql -U postgres -d gtfs -c "\copy routes_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_4.csv
+psql -U postgres -d gtfs -c "\copy shapes_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_4.csv
+psql -U postgres -d gtfs -c "\copy stops_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_4.csv
+psql -U postgres -d gtfs -c "\copy stop_times_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_4.csv
+psql -U postgres -d gtfs -c "\copy trips_4 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_4.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_4.csv
+psql -U postgres -d gtfs -c "\copy agency_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_5.csv
+psql -U postgres -d gtfs -c "\copy calendar_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_5.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_5.csv
+psql -U postgres -d gtfs -c "\copy routes_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_5.csv
+psql -U postgres -d gtfs -c "\copy shapes_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_5.csv
+psql -U postgres -d gtfs -c "\copy stops_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_5.csv
+psql -U postgres -d gtfs -c "\copy stop_times_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_5.csv
+psql -U postgres -d gtfs -c "\copy trips_5 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_5.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_5.csv
+psql -U postgres -d gtfs -c "\copy agency_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_6.csv
+psql -U postgres -d gtfs -c "\copy calendar_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_6.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_6.csv
+psql -U postgres -d gtfs -c "\copy routes_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_6.csv
+psql -U postgres -d gtfs -c "\copy shapes_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_6.csv
+psql -U postgres -d gtfs -c "\copy stops_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_6.csv
+psql -U postgres -d gtfs -c "\copy stop_times_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_6.csv
+psql -U postgres -d gtfs -c "\copy trips_6 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_6.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_6.csv
+psql -U postgres -d gtfs -c "\copy agency_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_7.csv
+psql -U postgres -d gtfs -c "\copy routes_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_7.csv
+psql -U postgres -d gtfs -c "\copy trips_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_7.csv
+psql -U postgres -d gtfs -c "\copy stops_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_7.csv
+psql -U postgres -d gtfs -c "\copy calendar_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_7.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_7.csv
+psql -U postgres -d gtfs -c "\copy shapes_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_7.csv
+psql -U postgres -d gtfs -c "\copy stop_times_7 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_7.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_7.csv
+psql -U postgres -d gtfs -c "\copy agency_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\agency_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\agency_8.csv
+psql -U postgres -d gtfs -c "\copy routes_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\routes_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\routes_8.csv
+psql -U postgres -d gtfs -c "\copy trips_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\trips_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\trips_8.csv
+psql -U postgres -d gtfs -c "\copy stops_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stops_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stops_8.csv
+psql -U postgres -d gtfs -c "\copy calendar_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_8.csv
+psql -U postgres -d gtfs -c "\copy calendar_dates_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\calendar_dates_8.csv
+psql -U postgres -d gtfs -c "\copy shapes_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\shapes_8.csv
+psql -U postgres -d gtfs -c "\copy stop_times_8 FROM 'c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_8.csv' DELIMITER ',' CSV HEADER"
+del c:\Users\An\Documents\GitHub\PTV\notebooks\stop_times_8.csv
